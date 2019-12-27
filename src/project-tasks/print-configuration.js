387a79fd1f4548072117660fbfda2a55719f4d35
Dan Howard <code_dh@svexpertise.com>
2019-12-27 15:16:10 -0800

    Add ability to install proxy middleware

--- src/project-tasks/print-configuration.js
+++ src/project-tasks/print-configuration.js
@@ -20,9 +20,30 @@ module.exports = {
           }
         } else {
           if (typeof value === 'object') {
+            let fnstr = '';
+            for (const [k, v] of Object.entries(value)) {
+              if (typeof v == 'function') {
+                if (fnstr) {
+                  fnstr += ',\n  ';
+                }
+                fnstr += `  "${k}": ${v}`;
+              }
+            }
+            if (fnstr) {
+              fnstr += '\n';
+            }
             value = JSON.stringify(value, null, '  ').split('\n').map((line) => {
               return '  ' + line;
             }).join('\n');
+            if (fnstr) {
+              if (value.length <= 4) {
+                fnstr = '\n  ' + fnstr;
+              } else {
+                value = value.substring(0, value.length-4) + ',' + value.substring(value.length-4);
+              }
+              fnstr = fnstr + '  ';
+            }
+            value = value.substring(0, value.length-1) + fnstr + '}';
           }
           if (colorsEnabled) {
             value = value.toString().cyan;
